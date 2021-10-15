a4b0cf99e3aea34cfdd54eb957f511321f9895a7
Jason Knoll <jason.p.knoll@gmail.com>
2021-10-14 22:54:18 -0700

    Add additional player settings

--- PlayerConfig.cs
+++ PlayerConfig.cs
@@ -8,6 +8,10 @@ public class PlayerConfig : UdonSharpBehaviour
 {
     public float jumpHeight = 3f;
     public float runSpeed = 4f;
+    public float walkSpeed = 2f;
+    public float strafeSpeed = 2f;
+    public float gravity = 1f;
+
     void Start()
     {
         SetDefaults();
@@ -20,5 +24,8 @@ public class PlayerConfig : UdonSharpBehaviour
         }
         Networking.LocalPlayer.SetJumpImpulse(jumpHeight);
         Networking.LocalPlayer.SetRunSpeed(runSpeed);
+        Networking.LocalPlayer.SetWalkSpeed(walkSpeed);
+        Networking.LocalPlayer.SetStrafeSpeed(strafeSpeed);
+        Networking.LocalPlayer.SetGravityStrength(gravity);
     }
 }
